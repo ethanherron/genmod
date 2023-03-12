333ff2c0e2f329c21b3e95817ee0d82997eeeeda
ethanherron <exouser@ethan-playground.js2local>
2023-03-12 21:53:02 +0000

    io

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import argparse
+import argparse, os
 import torch
-from data import *
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
+from lightning.pytorch.loggers import WandbLogger
+from torchvision import transforms
+from torchvision.datasets import MNIST
 
-from trainer import *
+from modules.trainers.DDPM import DDPM
 
 
 
@@ -16,13 +18,12 @@ def main(args):
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
-        
-    
     # ------------------------
     # 0 INIT DATAMODULE
     # ------------------------
-    train_dataset = SMIDataset('train')
-    val_dataset = SMIDataset('val')
+    tf = transforms.Compose([transforms.ToTensor()]) # mnist is already normalised 0 to 1
+    train_dataset = MNIST("./data", train=True, download=False, transform=tf)
+    val_dataset = MNIST("./data", train=False, download=False, transform=tf)
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, drop_last=True, num_workers=args.num_workers, pin_memory=True)
     val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers, pin_memory=True)
 
@@ -30,28 +31,22 @@ def main(args):
     # ------------------------
     # 1 INIT MODEL
     # ------------------------
+    model = DDPM()
 
     # ------------------------
     # 2 INIT TRAINER
     # ------------------------
-    logger = pl.loggers.TensorBoardLogger(save_dir, 
-                                          name="tensorboard_logs")
-
-    csv_logger = pl.loggers.CSVLogger(logger.save_dir, 
-                                      name=logger.name, 
-                                      version=logger.version)
+    wandb_logger = WandbLogger(project='genmod', 
+                               log_model='all')
 
     checkpoint = ModelCheckpoint(monitor='train_loss',
-                                 dirpath=logger.log_dir, 
-                                 filename='{epoch}-{step}',
                                  mode='min', 
                                  save_last=True)
 
     trainer = pl.Trainer(devices=args.gpu, 
-                         accelerator='gpu', 
-                         strategy='ddp',
+                         accelerator='gpu',
                          callbacks=[checkpoint],
-                         logger=[logger,csv_logger], 
+                         logger=wandb_logger, 
                          max_epochs=args.n_epochs, 
                          default_root_dir=save_dir, 
                          fast_dev_run=args.debug)
@@ -66,15 +61,15 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Trajectory Prediction')
-    parser.add_argument('--save_dir', default='.results/training/debug',
+    parser.add_argument('--save_dir', default='.results/training/DDPM',
                         type=str,help='path to directory for storing the checkpoints etc.')
-    parser.add_argument('-b','--batch_size', default=32, type=int,
+    parser.add_argument('-b','--batch_size', default=256, type=int,
                         help='Batch size')
-    parser.add_argument('-ep','--n_epochs', default=50, type=int,
+    parser.add_argument('-ep','--n_epochs', default=25, type=int,
                         help='Number of epochs')
-    parser.add_argument('-g','--gpu', default=2, type=int,
+    parser.add_argument('-g','--gpu', default=1, type=int,
                         help='num gpus')
-    parser.add_argument('--num_workers', default=24, type=int,
+    parser.add_argument('--num_workers', default=4, type=int,
                         help='num workers for data module.')
     parser.add_argument('--debug', default=False, type=bool,
                         help='fast_dev_run argument')
